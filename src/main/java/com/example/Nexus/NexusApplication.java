326c32a5405b9485fa97eb4e6ee582ace800b899
sahan542 <sahanrashmika542@gmail.com>
2024-02-18 15:21:51 +0530

    mongo db added

--- src/main/java/com/example/Nexus/NexusApplication.java
+++ src/main/java/com/example/Nexus/NexusApplication.java
@@ -2,12 +2,19 @@ package com.example.Nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class NexusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NexusApplication.class, args);
+		System.out.println("Hello");
+	}
+
+	@RequestMapping("/api")
+	public String sayHello(){
+		return "Hi Guys!";
 	}
 
 }
