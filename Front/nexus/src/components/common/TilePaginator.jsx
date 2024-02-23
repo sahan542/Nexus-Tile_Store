4817e1ca94564a6249b5aa0239203157ba8ec534
sahan542 <sahanrashmika542@gmail.com>
2024-02-24 00:35:22 +0530

    Tile Pagination button error fixed

--- Front/nexus/src/components/common/TilePaginator.jsx
+++ Front/nexus/src/components/common/TilePaginator.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 
 const TilePaginator = ({ currentPage, totalPages, onPageChange }) => {
-    const pageNumbers = Array.from({length : totalPages}, (_, i) => i+1)
-  return (
-    <nav>
-        <ul className='pagination, justify-content-center'>
-            {pageNumbers.map((pageNumber) => (
-                <li key={pageNumber}
-                    className={`page-item ${currentPage === pageNumber ? "active" : ""}`}
-                >
-                    <button className="page-link" onClick={() => onPageChange(pageNumber)}>
-                        {pageNumber}
-                    </button>
-
-                </li>
-            ))}
-
-        </ul>
-      
-    </nav>
-  )
+    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1)
+	return (
+		<nav aria-label="Page navigation">
+			<ul className="pagination justify-content-center">
+				{pageNumbers.map((pageNumber) => (
+					<li
+						key={pageNumber}
+						className={`page-item ${currentPage === pageNumber ? "active" : ""}`}>
+						<button onClick={() => onPageChange(pageNumber)} className="page-link">
+							{pageNumber}
+						</button>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
 }
 
 export default TilePaginator
