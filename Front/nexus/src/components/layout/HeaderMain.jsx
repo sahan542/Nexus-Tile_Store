8265d17834ac62dd8821ed0df198ddfd12eb5d9f
sahan542 <sahanrashmika542@gmail.com>
2024-02-25 20:53:04 +0530

    parallax , Tile service files are added

---
+++ Front/nexus/src/components/layout/HeaderMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderMain = () => {
+  return (
+    <header className='header-banner'>
+        <div className='overlay'></div>
+        <div className='animated-texts overlay-content'>
+            <h1>Welcome to <span className="hotel-color">Nexus Tiles</span></h1>
+            <h4>Experience the Premium Designs Collection in SriLanka</h4>
+        </div>
+      
+    </header>
+  )
+}
+
+export default HeaderMain
